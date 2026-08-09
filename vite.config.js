2009e381e46aafb557ba8bf0aef3a695aa67ae76
Carlo Dandan <carlodandan.personal@proton.me>
2026-08-09 17:14:22 +0000

    update build command
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://refract-strum-reconfirm.ngrok-free.dev',
+        target: 'https://lightanime-api.webbase.workers.dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
